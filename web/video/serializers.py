2c17ecac0ed32ae2d35a67e21dd202639f185120
trueB365 <dcmwangila@gmail.com>
2022-12-25 13:50:38 +0200

    wip: add missing web files

---
+++ web/video/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Video
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = ["video_link",
+                  "video_image",
+                  "video_length",
+                  "video_title",
+                  "video_update",
+                  "video_day",
+                  "video_keyword",
+                  "video_quality",
+                  "video_url",
+                  "video_source",
+                  "video_recomend",
+                  "createdAt",
+                  "updatedAt"]
